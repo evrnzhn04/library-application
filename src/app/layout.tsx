07cbbe1f35eb1643003fece11ef2178e13452bcc
evrnzhn04 <110024215+evrnzhn04@users.noreply.github.com>
2025-09-28 08:16:04 +0300

    v0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./navbar/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-600`}
       >
-        {children}
+        <div className="flex flex-1 flex-row">
+          <Navbar/>
+
+          <div className="flex-1">
+            {children}
+          </div>
+        </div>        
       </body>
     </html>
   );
---
+++ src/app/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-64 min-h-screen bg-slate-700'>
+        <div className='p-6'>
+            <h1 className='text-3xl font-bold text-slate-200 text-center'>Library</h1>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
